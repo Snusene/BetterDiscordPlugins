10906d67308a09626f862aaa8d8388e6865ed2a6
Snusene <nystadp@gmail.com>
2026-01-23 23:05:32 +0100

    1.0.2

--- ScrollToLatest/ScrollToLatest.plugin.js
+++ ScrollToLatest/ScrollToLatest.plugin.js
@@ -3,30 +3,30 @@
  * @author Snues
  * @authorId 98862725609816064
  * @description Auto marks channels as read when you enter them so that it scrolls to the last message.
- * @version 1.0.0
+ * @version 1.0.2
  * @website https://github.com/Snusene/BetterDiscordPlugins/tree/main/ScrollToLatest
  * @source https://raw.githubusercontent.com/Snusene/BetterDiscordPlugins/main/ScrollToLatest/ScrollToLatest.plugin.js
  */
 
 module.exports = class ScrollToLatest {
-    start() {
-        this.Dispatcher = BdApi.Webpack.getByKeys("dispatch", "subscribe");
-        this.AckUtils = BdApi.Webpack.getByKeys("ack");
-        this.lastChannelId = null;
+  start() {
+    this.Dispatcher = BdApi.Webpack.getStore("UserStore")._dispatcher;
+    this.AckUtils = BdApi.Webpack.getByKeys("ack");
+    this.lastChannelId = null;
 
-        this.onSwitch = this.onSwitch.bind(this);
-        this.Dispatcher?.subscribe("CHANNEL_SELECT", this.onSwitch);
-    }
+    this.onSwitch = this.onSwitch.bind(this);
+    this.Dispatcher.subscribe("CHANNEL_SELECT", this.onSwitch);
+  }
 
-    stop() {
-        this.Dispatcher?.unsubscribe("CHANNEL_SELECT", this.onSwitch);
-    }
+  stop() {
+    this.Dispatcher.unsubscribe("CHANNEL_SELECT", this.onSwitch);
+  }
 
-    onSwitch(event) {
-        const channelId = event?.channelId;
-        if (!channelId || channelId === this.lastChannelId) return;
+  onSwitch(event) {
+    const channelId = event?.channelId;
+    if (!channelId || channelId === this.lastChannelId) return;
 
-        this.lastChannelId = channelId;
-        this.AckUtils?.ack?.(channelId);
-    }
+    this.lastChannelId = channelId;
+    this.AckUtils.ack(channelId);
+  }
 };
