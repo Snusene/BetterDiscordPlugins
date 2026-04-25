6ac80dce71d982108c79193d8d691f4744ed1de4
Snues <81967520+Snusene@users.noreply.github.com>
2026-04-25 19:25:45 +0200

    1.0.4

--- ScrollToLatest/ScrollToLatest.plugin.js
+++ ScrollToLatest/ScrollToLatest.plugin.js
@@ -3,8 +3,9 @@
  * @author Snues
  * @authorId 98862725609816064
  * @description Scroll to the last message when entering a channel.
- * @version 1.0.3
- * @source https://raw.githubusercontent.com/Snusene/BetterDiscordPlugins/main/ScrollToLatest/ScrollToLatest.plugin.js
+ * @version 1.0.4
+ * @invite xp2f3YFKMY
+ * @source https://github.com/Snusene/BetterDiscordPlugins/tree/main/ScrollToLatest
  * @donate https://ko-fi.com/snues
  */
 
@@ -12,6 +13,7 @@ module.exports = class ScrollToLatest {
   start() {
     this.Dispatcher = BdApi.Webpack.Stores.UserStore._dispatcher;
     this.AckUtils = BdApi.Webpack.getByKeys("ack");
+    this.ReadStateStore = BdApi.Webpack.getStore("ReadStateStore");
     this.lastChannelId = null;
 
     this.onSwitch = this.onSwitch.bind(this);
@@ -27,6 +29,8 @@ module.exports = class ScrollToLatest {
     if (!channelId || channelId === this.lastChannelId) return;
 
     this.lastChannelId = channelId;
-    this.AckUtils.ack(channelId);
+    const lastId = this.ReadStateStore?.lastMessageId(channelId);
+    this.AckUtils.ack(channelId, undefined, true, true, lastId);
+    this.Dispatcher.dispatch({ type: "CHANNEL_LOCAL_ACK", channelId });
   }
 };
